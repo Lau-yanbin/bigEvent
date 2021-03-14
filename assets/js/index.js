d8bcdc66e96876a7548c9149b794af2f5b768970
LiuYanbin <205999457@qq.com>
2021-03-14 11:23:20 +0800

    完成首页优化功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,16 +2,15 @@
 getUserInfo();
 function getUserInfo() {
     // 发送axios请求获取数据  渲染昵称和头像
-    axios.get('http://api-breakingnews-web.itheima.net/my/userinfo', {
+    axios.get('/my/userinfo', {
         // 发送请求头信息
-        headers: {
-            // 传递身份信息
-            Authorization: localStorage.getItem('token')
-        }
+        // headers: {
+        //     // 传递身份信息
+        //     Authorization: localStorage.getItem('token')
+        // }
     }).then(function (res) {
         // 获取用户的基本数据
         let info = res.data.data;
-        console.log(info);
         //判断昵称是否为空  如果为空就用户名代替
         let name = info.nickname || info.username;
         // 欢迎你 用户名（优先使用nickname、没有的话，使用username）
@@ -26,3 +25,18 @@ function getUserInfo() {
         }
     })
 }
+$(function () {
+    // 加载layer模块
+    let layer = layui.layer;
+    //点击退出
+    $('#btnLogout').click(function () {
+        // 确认退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 确认退出 删除认证信息 'token', 并且返回登录页面
+            localStorage.removeItem('token');
+            location.href = 'login.html';
+            layer.close(index);
+        });
+    })
+})
